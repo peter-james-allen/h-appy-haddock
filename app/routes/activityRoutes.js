00bd66fb905180a2cc5249ed4e7fa2ae20918d72
Peter Allen <peter_james_allen@hotmail.com>
2021-04-15 20:56:32 +0100

    updated package.json

--- app/routes/activityRoutes.js
+++ app/routes/activityRoutes.js
@@ -8,11 +8,11 @@ app.get('/activities', async (request, response) => {
   const mains = await activityModel.find({ size: 'main' });
   const desserts = await activityModel.find({ size: 'dessert' });
   const activities = {
-    'nibbles': nibbles,
-    'appetisers': appetisers,
-    'mains': mains,
-    'desserts': desserts
-  }
+    nibbles: nibbles,
+    appetisers: appetisers,
+    mains: mains,
+    desserts: desserts,
+  };
 
   try {
     response.send(activities);
@@ -25,6 +25,27 @@ app.post('/activities', async (request, response) => {
   const activities = await activityModel.find(request.body);
   try {
     response.send(activities);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.patch('/activities/:id', async (request, response) => {
+  try {
+    await activityModel.findByIdAndUpdate(request.params.id, request.body);
+    await activityModel.save();
+    response.send(activity);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.delete('/activities/:id', async (request, response) => {
+  try {
+    const activity = await activityModel.findByIdAndDelete(request.params.id);
+
+    if (!activity) response.status(404).send('No item found');
+    response.status(200).send();
   } catch (error) {
     response.status(500).send(error);
   }
